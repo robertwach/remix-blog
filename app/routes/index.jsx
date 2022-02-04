fa7881c8463196c032f35c692089e748f591bd7f
“Robert” <robertwach@Roberts-MacBook-Pro.local>
2022-02-04 17:14:34 +0300

    first commit

---
+++ app/routes/index.jsx
@@ -0,0 +1,19 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome </h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+      
+      </ul>
+    </div>
+  );
+}
